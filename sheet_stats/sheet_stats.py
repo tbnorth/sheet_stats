585c4b2bd03259520f77f9e3db48e778d1ce03a6
Terry N. Brown <terry_n_brown@yahoo.com>
2016-12-16 15:37:32 -0600

    multi-processing version

--- sheet_stats/sheet_stats.py
+++ sheet_stats/sheet_stats.py
@@ -6,8 +6,10 @@ requires openpyxl and numpy
 Terry N. Brown, terrynbrown@gmail.com, Fri Dec 16 13:20:47 2016
 """
 
+import csv
 import argparse
 import glob
+import multiprocessing
 import os
 import sys
 from collections import namedtuple, defaultdict
@@ -50,6 +52,65 @@ def get_options(args=None):
 
     return opt
 
+def proc_file(filepath):
+
+    print filepath
+
+    wb = load_workbook(filename=filepath, read_only=True)
+    sheets = wb.get_sheet_names()
+    ws = wb[sheets[0]]
+    row0 = next(ws.rows)
+    fields = [i.value for i in row0]
+
+    cols = len(fields)
+
+    n = np.zeros(cols, dtype=int)
+    sums = np.zeros(cols)
+    sumssq = np.zeros(cols)
+    blank = np.zeros(cols, dtype=int)
+    bad = np.zeros(cols, dtype=int)
+    mins = np.zeros(cols) + np.nan
+    maxs = np.zeros(cols) + np.nan
+
+    rows = 0
+
+    for row in ws.rows:
+        if rows % 1000 == 0:
+            print rows
+            if os.path.exists("STOP"):
+                return
+        rows += 1
+
+        for cell_n, cell in enumerate(row):
+            if cell.value is None or str(cell.value).strip() == '':
+                blank[cell_n] +=1
+            else:
+                try:
+                    x = float(cell.value)
+                    sums[cell_n] += x
+                    sumssq[cell_n] += x*x
+                    n[cell_n] += 1
+                    if np.isnan(mins[cell_n]):
+                        mins[cell_n] = x
+                    else:
+                        mins[cell_n] = min(mins[cell_n], x)
+                    if np.isnan(maxs[cell_n]):
+                        maxs[cell_n] = x
+                    else:
+                        maxs[cell_n] = max(maxs[cell_n], x)
+                except ValueError:
+                    bad[cell_n] += 1
+
+    assert sum(n) + sum(blank) + sum(bad) == rows * len(fields)
+
+    ans = []
+    for i in range(len(fields)):
+        ans.append([
+            filepath, fields[i], sums[i], sumssq[i],
+            mins[i], maxs[i], n[i], blank[i], bad[i]
+        ])
+    return ans
+
 def main():
 
     opt = get_options()
@@ -59,58 +120,21 @@ def main():
     for filepath in opt.files:
         files.extend(glob.glob(filepath))
 
-    for filepath in files:
-        print filepath
-        wb = load_workbook(filename=filepath, read_only=True)
-        sheets = wb.get_sheet_names()
-        ws = wb[sheets[0]]
-        row0 = next(ws.rows)
-        fields = [i.value for i in row0]
-        
-        cols = len(fields)
-        
-        n = np.zeros(cols, dtype=int)
-        sums = np.zeros(cols)
-        sumssq = np.zeros(cols)
-        blank = np.zeros(cols, dtype=int)
-        bad = np.zeros(cols, dtype=int)
-        mins = np.zeros(cols) + np.nan
-        maxs = np.zeros(cols) + np.nan
+    pool = multiprocessing.Pool(multiprocessing.cpu_count()-1)
 
-        rows = 0
+    answers = pool.map(proc_file, files)
 
-        for row in ws.rows:
-            rows += 1
-            if rows % 1000 == 0:
-                print rows
+    fields = [
+        'file', 'field', 'sum', 'sumsq', 'min', 'max', 'n', 'blank', 'bad'
+    ]
 
-            for cell_n, cell in enumerate(row):
-                if cell.value is None or str(cell.value).strip() == '':
-                    blank[cell_n] +=1
-                else:
-                    try:
-                        x = float(cell.value)
-                        sums[cell_n] += x
-                        sumssq[cell_n] += x*x
-                        n[cell_n] += 1
-                        if np.isnan(mins[cell_n]):
-                            mins[cell_n] = x
-                        else:
-                            mins[cell_n] = min(mins[cell_n], x)
-                        if np.isnan(maxs[cell_n]):
-                            maxs[cell_n] = x
-                        else:
-                            maxs[cell_n] = max(maxs[cell_n], x)
-                    except ValueError:
-                        bad[cell_n] += 1
-
-            if rows > 3000:
-                break
-
-        assert sum(n) + sum(blank) + sum(bad) == rows * len(fields)
-
-        for i in range(len(fields)):
-            print fields[i], sums[i], sumssq[i], mins[i], maxs[i], n[i], blank[i], bad[i]
+    writer = csv.writer(open("stats.csv", 'wb'))
+    writer.writerow(fields)
+    for answer in answers:
+        assert len(answer[0]) == len(fields), (len(answer[0]), len(fields))
+        writer.writerows(answer)
 
 if __name__ == '__main__':
     main()
+
+
